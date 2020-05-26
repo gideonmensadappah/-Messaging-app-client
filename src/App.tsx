ed4bfff8a405eae1a438e7825613cfcf4b625b82
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-26 08:36:08 +0300

    - created my app theme and added to theme prover - styled the content

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,51 @@
 import React from "react";
 import "./App.css";
-import { Header } from "./header/navbarHeader";
+import { Header } from "./components/header/navbarHeader";
+import { SideBar } from "./components/sidebar/sidebarList";
+import {
+  styled,
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+
 type Props = {};
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "'Roboto', sans-serif",
+  },
+});
+const AppContainer = styled(Box)({
+  display: "flex",
+  height: "100vh",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+});
+const Content = styled(Box)({
+  display: "flex",
+  flex: 1,
+  flexDirection: "row",
+  alignItems: "stretch",
+});
+
+const sideBarItem = {
+  contactName: "gideon",
+  lastMessage: "hello user",
+};
 const App: React.FC<Props> = () => {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppContainer>
+        <Header />
+        <Content>
+          <SideBar
+            contactName={sideBarItem.contactName}
+            lastMessage={sideBarItem.lastMessage}
+          />
+        </Content>
+      </AppContainer>
+    </ThemeProvider>
   );
 };
 
