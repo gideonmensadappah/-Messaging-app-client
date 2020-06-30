c7dc9f4897bcf50b3ed1760900ebef6ce674dc86
Gideon Mensa <gideon123ewq@gmail.com>
2020-06-30 10:29:20 +0300

    implemented chats type get all current user chats list and and added to side bar

--- src/components/sidebar/sidebarList.tsx
+++ src/components/sidebar/sidebarList.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./sideBar.css";
 import { SideBarItem } from "./sidebarItem";
-
+import { getCurrentUserMessagesAsync } from "../../functionsHelpers/myFunctions";
 type Props = {
   contactName: string;
   lastMessage: string;
 };
 
+type chatsState = {
+  _id: string;
+  usersId: Array<string>;
+  length: number;
+};
+
 export const SideBar: React.FC<Props> = ({ lastMessage, contactName }) => {
+  const [chatsList, setChatsList] = useState<chatsState>();
+  const [length, setLength] = useState<number | any>();
+  useEffect(() => {
+    getCurrentUserMessagesAsync("24").then((chats: chatsState) => {
+      setLength(chats.length);
+      setChatsList(chats);
+    });
+  }, []);
   return (
     <div className="container">
-      {Array.from({ length: 20 }, () => (
-        <SideBarItem contactName={contactName} lastMessage={lastMessage} />
-      ))}
+      {chatsList?.length
+        ? Array.from({ length: length }, () => (
+            <SideBarItem contactName={contactName} lastMessage={lastMessage} />
+          ))
+        : "No Chats!"}
     </div>
   );
 };
