bf7f6cb12e58fe468dd745a046bd95ad231c7ecf
Gideon Mensa <gideon123ewq@gmail.com>
2020-07-02 21:34:00 +0300

    - requests to server

--- src/functionsHelpers/myFunctions.tsx
+++ src/functionsHelpers/myFunctions.tsx
@@ -1,5 +1,46 @@
 const REQUEST_URL = "http://localhost:5151/messages/";
+
+export type Chat = {
+  _id: string;
+  usersId: Array<string>;
+  length: number;
+};
 export const getCurrentUserMessagesAsync = async (currentUserId: string) => {
   let res = await fetch(`${REQUEST_URL}user/${currentUserId}`);
-  return await res.json();
+  const chats: Array<Chat> = await res.json();
+  return chats;
+};
+
+//
+export type ChatPayload = {
+  currentUserId: string;
+  requestedUserId: string;
+};
+export const createNewChatAsync = async (payload: ChatPayload) => {
+  const res = await fetch(`${REQUEST_URL}create-new-chat`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+  return res.json();
+};
+
+export type User = {
+  uid: string | undefined;
+  firstName: string;
+  lastName: string;
+  phone: number;
+};
+export const registerUser = async (payload: User) => {
+  const res = await fetch(`http://localhost:5151/users/new-user`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  return res.json();
 };
