4fbd09cfed96801ad739339990c62cf8a152b9d8
Gideon Mensa <gideon123ewq@gmail.com>
2020-07-11 13:18:38 +0300

    Refactored message

---
+++ src/components/message.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import cn from "classnames";
+import styles from "./message.module.scss";
+import Avatar from "@material-ui/core/Avatar";
+type Props = {
+  message: Message;
+  currentUserId: string;
+};
+export type Message = {
+  id: string;
+  avatar: string;
+  userId: string | null;
+  chatId: string | null;
+  text: string | null;
+  createdAt: Date;
+};
+export const MessageView: React.FC<Props> = ({
+  message: { id, userId, chatId, avatar, text, createdAt },
+  currentUserId,
+}) => {
+  const isOwnMessage = userId === currentUserId;
+  return (
+    <div
+      className={cn(
+        styles.messageContainer,
+        isOwnMessage ? styles.ownMessage : styles.userMessage
+      )}
+    >
+      {!isOwnMessage ? <Avatar className={styles.avatar} src={avatar} /> : null}
+      <div className={styles.message}>{text}</div>
+    </div>
+  );
+};
