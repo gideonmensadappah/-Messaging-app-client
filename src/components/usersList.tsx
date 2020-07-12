64be84b2f76a829f4f100f6678be08fc6e50c7e6
Gideon Mensa <gideon123ewq@gmail.com>
2020-07-12 15:41:04 +0300

    Suggestion users list in db

---
+++ src/components/usersList.tsx
@@ -0,0 +1,56 @@
+import React, { useCallback } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from "@material-ui/styles";
+import { User } from "./newChat";
+import {
+  createNewChatAsync,
+  ChatPayload,
+} from "../functionsHelpers/myFunctions";
+type Props = {
+  suggestion: User;
+  currentUserId: string;
+  setShowList: () => void;
+};
+const useStyle = makeStyles({
+  avatar: {
+    backgroundColor: "#adc852",
+  },
+});
+export const UsersList: React.FC<Props> = ({
+  suggestion: { _id, firstName, lastName, phone },
+  currentUserId,
+  setShowList,
+}) => {
+  const onUserClicked = useCallback(
+    (event, _id, firstName) => {
+      const res = globalThis.confirm(
+        `Do you want to start chat with ${firstName}`
+      );
+      if (res) {
+        const payload: ChatPayload = {
+          currentUserId,
+          requestedUserId: _id,
+        };
+        alert(`new chat with ${firstName}`);
+        createNewChatAsync(payload);
+        setShowList();
+      }
+    },
+    [currentUserId, setShowList]
+  );
+  const classes = useStyle();
+  return (
+    <div
+      className="message"
+      onClick={(event) => onUserClicked(event, _id, firstName)}
+    >
+      <div className="details">
+        <Avatar className={classes.avatar}>H</Avatar>
+        <div className="metadata">
+          <div className="user-name">{firstName}</div>
+          <div className="message-preview">{lastName + " " + phone}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
