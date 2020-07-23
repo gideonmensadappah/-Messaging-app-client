a8dc31da0749eb5ba7cdb9483191d4f8ecca56e2
Gideon Mensa <gideon123ewq@gmail.com>
2020-07-23 20:01:07 +0300

    - refactored the code - removed unused code

--- src/functionsHelpers/myFunctions.tsx
+++ src/functionsHelpers/myFunctions.tsx
@@ -1,5 +1,26 @@
 const { REACT_APP_BACKEND_URL } = process.env;
 
+export type Chat = {
+  _id: string;
+  usersId: Array<string>;
+  length: number;
+};
+export type ChatList = {
+  _id: string;
+  usersId: Array<string>;
+  chatsdetails: Array<User>;
+};
+export type ChatPayload = {
+  currentUserId: string;
+  requestedUserId: string;
+};
+export type User = {
+  uid: string | undefined;
+  firstName: string;
+  lastName: string;
+  phone: number;
+};
+
 export const deleteChat = async (chatId: string) => {
   const res = await fetch(`${REACT_APP_BACKEND_URL}/chat/${chatId}`, {
     method: "DELETE",
@@ -7,27 +28,17 @@ export const deleteChat = async (chatId: string) => {
   return res.json();
 };
 
-export const getUsersList = async (userInput: string) => {
+export const getAutoCompleteUsersList = async (userInput: string) => {
   const res = await fetch(`${REACT_APP_BACKEND_URL}/search?query=${userInput}`);
   return res.json();
 };
 
-export type Chat = {
-  _id: string;
-  usersId: Array<string>;
-  length: number;
-};
-
 export const getCurrentUserChats = async (currentUserId: string) => {
   const res = await fetch(`${REACT_APP_BACKEND_URL}/chats/${currentUserId}`);
-  const chats: Array<Chat> = await res.json();
+  const chats: Array<ChatList> = await res.json();
   return chats;
 };
 
-export type ChatPayload = {
-  currentUserId: string;
-  requestedUserId: string;
-};
 export const createNewChat = async (payload: ChatPayload) => {
   const res = await fetch(`${REACT_APP_BACKEND_URL}/chat`, {
     method: "POST",
@@ -39,12 +50,6 @@ export const createNewChat = async (payload: ChatPayload) => {
   return res.json();
 };
 
-export type User = {
-  uid: string | undefined;
-  firstName: string;
-  lastName: string;
-  phone: number;
-};
 export const registerUser = async (payload: User) => {
   const res = await fetch(`${REACT_APP_BACKEND_URL}/users/new-user`, {
     method: "POST",
